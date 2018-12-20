e8d103e9daaa2f904fb9b789c7ce84179b4f339e
GabrielSegatto <gabriel.segatto58@gmail.com>
2018-12-20 10:15:21 -0200

    att index home

--- aula8/test.js
+++ aula8/test.js
@@ -2,7 +2,7 @@ let x = 0;
 x = 11;
 console.log(x);
 
-if ((x = 12) ||(x = 10)){
+if ((x = 12) || (x = 10)){
     console.log('Correto');
 } else{
     console.log('Acho que não');
--- aula9/funcoes.js
+++ aula9/funcoes.js
@@ -1,35 +1,7 @@
-function somar (num1, num2){
-    console.log( num1 + num2);
-}
-somar(150.5, 2.5);
-
-function falar(texto){
-    console.log(texto)
+function falar(texto) {
+    console.log(texto);
 }
 
+let string = 'Eu sou um texto';
 
-
-
-
-
-
-
-
-
-
-
-falar("Eu sou um texto")
-
-
-
-
-
-
-
-// function falar(texto) {
-//     console.log(texto);
-// }
-
-// let string = 'Eu sou um texto';
-
-// falar(string);
+falar(string);
